96b29d6aa40292626b9f0b7d4aa92ed231c97077
Deepanshu Gupta <DEEPANSHUGUPTA899@GMAIL.COM>
2023-09-14 18:09:36 +0530

    Update mailer.ts

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -41,8 +41,8 @@ export const sendEmail = async({email,emailType,userId}:any)=>{
             from : "deepanshuguptaa617@gmail.com",
             to:email,
             subject: emailType==='VERIFY'?"Verify your email":"Reset your password",
-            html: `<p>Click <a href="http://localhost:3000/forgetpassword?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
-            or copy and paste the link below in your browser. <br> http://localhost:3000/forgetpassword?token=${hashedToken}
+            html: `<p>Click <a href="${process.env.DOMAIN}/forgetpassword?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
+            or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/forgetpassword?token=${hashedToken}
             </p>`
           }
       }
